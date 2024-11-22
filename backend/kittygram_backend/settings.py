b9701f318bef75750b20759bfe32ef36a8b68208
Dmitry Alexandrov <alexandrov.dm91@gmail.com>
2024-11-22 10:04:59 +0300

    all done 6?

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['ald-kitty.zapto.org', '89.169.175.34', 'localhost']
+ALLOWED_HOSTS = ['ald-kitty.zapto.org', '89.169.175.34', '127.0.0.1', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
