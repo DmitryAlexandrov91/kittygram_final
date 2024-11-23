1847e5e4b161355b625863e1f5188748fcc3709d
Dmitry Alexandrov <alexandrov.dm91@gmail.com>
2024-11-23 14:01:44 +0300

    Flake8 fix

--- backend/kittygram_backend/utils.py
+++ backend/kittygram_backend/utils.py
@@ -3,11 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 def debug_bool_check():
     if os.getenv('DEBUG'):
         if os.getenv('DEBUG').lower() == 'true':
             return True
     return False
+
 
 def get_allowed_hosts():
     allowed_hosts = os.getenv('ALLOWED_HOSTS', 'localhost', '127.0.0.1')
