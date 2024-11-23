fc617938c0068f5edcef7b610b56e279db760758
Dmitry Alexandrov <alexandrov.dm91@gmail.com>
2024-11-23 13:57:33 +0300

    Utils get allowed hosts fix

--- backend/kittygram_backend/utils.py
+++ backend/kittygram_backend/utils.py
@@ -3,16 +3,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 def debug_bool_check():
     if os.getenv('DEBUG'):
         if os.getenv('DEBUG').lower() == 'true':
             return True
     return False
 
-
 def get_allowed_hosts():
-    allowed_hosts = os.getenv('ALLOWED_HOSTS')
-    if not allowed_hosts or allowed_hosts == '':
-        return ['localhost', '127.0.0.1']
-    return allowed_hosts.split(', ')
+    allowed_hosts = os.getenv('ALLOWED_HOSTS', 'localhost', '127.0.0.1')
+    return [host.strip() for host in allowed_hosts.split(',')]
