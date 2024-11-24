a0737814782ca86413a4753af4e0ae6a29d6dd8c
Dmitry Alexandrov <alexandrov.dm91@gmail.com>
2024-11-24 17:37:31 +0300

    Flake8 fix in utils

--- backend/kittygram_backend/utils.py
+++ backend/kittygram_backend/utils.py
@@ -7,7 +7,7 @@ load_dotenv()
 def debug_bool_check():
     debug = os.getenv('DEBUG', 'False')
     return debug.lower() == 'true'
-    
+
 
 def get_allowed_hosts():
     allowed_hosts = os.getenv('ALLOWED_HOSTS', 'localhost, 127.0.0.1')
